30b18af7dcb90cae39298654e40c1f5a43570265
Sören Giller <sgiller@techfak.uni-bielefeld.de>
2017-11-21 16:09:17 +0100

    feat(dockerfile): added Dockerfile & requirements.txt

--- gen-py/VirtualMachineService/VirtualMachineServer.py
+++ gen-py/VirtualMachineService/VirtualMachineServer.py
@@ -13,7 +13,7 @@ from thrift.server import TServer
 if __name__ == '__main__':
     handler=VirtualMachineHandler()
     processor=Processor(handler)
-    transport = TSocket.TServerSocket(port=9090)
+    transport = TSocket.TServerSocket(host='0.0.0.0', port=9090)
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
     server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
