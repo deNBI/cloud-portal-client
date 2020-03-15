8907c34bd41eb427ab54adb263b6c4402e96c6ea
David Weinholz <david-weinholz@web.de>
2020-03-15 12:48:11 +0100

    updated script

--- VirtualMachineService/scripts/bash/mount.sh
+++ VirtualMachineService/scripts/bash/mount.sh
@@ -3,22 +3,27 @@ sudo touch test
 declare -a volumes=VOLUME_IDS
 declare -a paths=VOLUME_PATHS
 ITER=0
-for id in "${volumes[@]}"
-do
-  while :
-  do
-    if [ -e /dev/disk/by-id/"$id" ];
-    then
-      echo "volume  ""$id"" found";
-      break;
+ABORT_COUNTER=0
+for id in "${volumes[@]}"; do
+  while :; do
+    if [ -e /dev/disk/by-id/"$id" ]; then
+      echo "volume  ""$id"" found"
+      ABORT_COUNTER=0
+      break
     else
-      echo "no volume ""$id"" found";
+      echo "no volume ""$id"" found"
+      if [ $ABORT_COUNTER -gt 12 ]; then
+        exit 1
+      fi
+      ((ABORT_COUNTER++))
+
       sleep 10
-    fi;
+    fi
   done
- cd /dev/disk/by-id || exit
- sudo mkfs.ext4 "$id"
- sudo mkdir -p /mnt/${paths[ITER]}
- sudo chmod 777 /mnt/${paths[ITER]}/
- sudo  mount "$id" /mnt/${paths[ITER]}
+  cd /dev/disk/by-id || exit
+  sudo mkfs.ext4 "$id"
+  sudo mkdir -p /mnt/${paths[ITER]}
+  sudo chmod 777 /mnt/${paths[ITER]}/
+  sudo mount "$id" /mnt/${paths[ITER]}
+  ((ITER++))
 done
