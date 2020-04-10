3154b766a68e8d9e12c90e313da8fc46122388c6
David Weinholz <david-weinholz@web.de>
2020-04-10 11:57:03 +0200

    added scripts

---
+++ scripts/generate_new_test_pems.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect
+echo Creating new dir "new_pem"
+mkdir new_pem
+cd new_pem
+openssl req -new -x509 -nodes -days 3000 -out server.crt -keyout server.key -subj "/C=DE/ST=./L=./O=./CN=$1"  
+
+openssl x509 -in server.crt -text > CA.pem
+cat server.crt server.key > server.pem
+openssl pkcs12 -export -clcerts -in server.crt -inkey server.key -out server.p12  -passout pass:thrift
+openssl genrsa -out client.key 
+openssl req -new -nodes -key client.key -out client.csr  -subj "/C=DE/ST=./L=./O=./CN=$1" -passin pass:thrift
+openssl x509 -req -days 3000 -in client.csr -CA CA.pem -CAkey server.key -set_serial 01 -out client.crt
+openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.p12  -passout pass:thrift
+openssl pkcs12 -in client.p12 -out client.pem -clcerts  -passin pass:thrift -passout pass:thrift
+openssl rsa -in client.pem -out client_no_pass.pem   -passin pass:thrift
+sed -i '/-----BEGIN ENCRYPTED PRIVATE KEY-----/Q' client.pem
+cat client_no_pass.pem >> client.pem
+find . -type f -not -name '*.pem' -print0  | xargs -0 rm
+rm client_no_pass.pem
