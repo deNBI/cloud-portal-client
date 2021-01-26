cfe294401ab88f7c6efa5f51c374cacc663912ee
David Weinholz <david-weinholz@web.de>
2021-01-26 17:13:47 +0100

    removed test.txt

--- VirtualMachineService/scripts/bash/add_keys_to_authorized.sh
+++ VirtualMachineService/scripts/bash/add_keys_to_authorized.sh
@@ -3,6 +3,6 @@ declare -a keys_to_add=KEYS_TO_ADD
 echo "Found keys: ${#keys_to_add[*]}"
 for ix in ${!keys_to_add[*]}
 do
-    printf "\n%s\n" "${keys_to_add[$ix]}" >> /home/ubuntu/.ssh/authorized_keys
+    printf "\n%s" "${keys_to_add[$ix]}" >> /home/ubuntu/.ssh/authorized_keys
 
 done
