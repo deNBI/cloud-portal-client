be763257723217519089f09064559019cf089749
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2022-06-01 11:25:17 +0200

    still  encoding without a string argument

--- VirtualMachineService/VirtualMachineHandler.py
+++ VirtualMachineService/VirtualMachineHandler.py
@@ -1490,10 +1490,11 @@ class VirtualMachineHandler(Iface):
                 headers={"X-API-KEY": self.FORC_API_KEY},
                 verify=True,
             )
+            logging.error(response.content.decode())
             if response.status_code == 401:
                 return ["Error: 401"]
             else:
-                return response.json()
+                return json.loads(response.content.decode())
         except Timeout as e:
             LOG.info(msg=f"Get users for backend timed out. {e}")
             return []
