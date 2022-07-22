74dd81a4196f409e269fadb8ff3beca8d47cf727
dweinholz <dweinholz@users.noreply.github.com>
2022-07-22 08:36:24 +0000

    fix(Linting):blacked code

--- VirtualMachineService/VirtualMachineHandler.py
+++ VirtualMachineService/VirtualMachineHandler.py
@@ -236,7 +236,9 @@ class VirtualMachineHandler(Iface):
                 self.BIBIGRID_ANSIBLE_ROLES = cfg["bibigrid"].get(
                     "ansibleGalaxyRoles", []
                 )
-                LOG.info(f"Loaded Ansible Galaxy Roles for Bibigrid:\n {self.BIBIGRID_ANSIBLE_ROLES}")
+                LOG.info(
+                    f"Loaded Ansible Galaxy Roles for Bibigrid:\n {self.BIBIGRID_ANSIBLE_ROLES}"
+                )
 
                 LOG.info(msg=f"Bibigrd url loaded: {self.BIBIGRID_URL}")
             except Exception as e:
@@ -1973,8 +1975,8 @@ class VirtualMachineHandler(Iface):
         json_resp = response.json(strict=False)
         json_resp["log"] = str(json_resp.get("log", ""))
         json_resp["msg"] = str(json_resp.get("msg", ""))
-        msg=json_resp["msg"]
-        info=json_resp.get("info","")
+        msg = json_resp["msg"]
+        info = json_resp.get("info", "")
         LOG.info(f"Cluster {cluster_id} status: - {msg} | {info}")
         return json_resp
 
@@ -2052,7 +2054,6 @@ class VirtualMachineHandler(Iface):
         pub_key,
         project_name,
         project_id,
-
     ):
         LOG.info(f"Add machine to [{name}] {cluster_id} - {key_name}")
         image = self.get_image(image=image)
