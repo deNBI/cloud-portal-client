ab16ddf4ec18d33943cf7b3993573707b894455a
dweinholz <dweinholz@users.noreply.github.com>
2022-10-18 12:47:41 +0000

    fix(Linting):blacked code

--- VirtualMachineService/VirtualMachineHandler.py
+++ VirtualMachineService/VirtualMachineHandler.py
@@ -2185,15 +2185,13 @@ class VirtualMachineHandler(Iface):
             )
         )
 
-        server=self.conn.get_server_by_id(openstack_id)
+        server = self.conn.get_server_by_id(openstack_id)
         LOG.info(server)
         if server is None:
             LOG.exception(f"Instance {openstack_id} not found")
             raise serverNotFoundException
         try:
-            snapshot_munch = self.conn.create_image_snapshot(
-                server=server, name=name
-            )
+            snapshot_munch = self.conn.create_image_snapshot(server=server, name=name)
         except ConflictException as e:
             LOG.exception(f"Create snapshot {openstack_id} error: {e}")
 
