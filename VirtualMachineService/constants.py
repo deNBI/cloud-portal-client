7d02a3ec7656dda60bce44205d08bb2b7483a5ef
dweinholz <david-weinholz@web.de>
2022-11-11 12:16:04 +0100

    started implemenitng resenv for cluster

--- VirtualMachineService/constants.py
+++ VirtualMachineService/constants.py
@@ -6,17 +6,10 @@
 #  options string: py
 #
 
-from thrift.Thrift import (
-    TType,
-    TMessageType,
-    TFrozenDict,
-    TException,
-    TApplicationException,
-)
+from thrift.Thrift import TType, TMessageType, TFrozenDict, TException, TApplicationException
 from thrift.protocol.TProtocol import TProtocolException
 from thrift.TRecursive import fix_spec
 
 import sys
 from ttypes import *
-
 VERSION = "1.0.0"
