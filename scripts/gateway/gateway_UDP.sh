27e367e0b9f1714882d4e677cad8589035267b28
dweinholz <david-weinholz@web.de>
2022-11-22 13:33:29 +0100

    updated udp security group

--- scripts/gateway/gateway_UDP.sh
+++ scripts/gateway/gateway_UDP.sh
@@ -13,9 +13,9 @@ for ((n=1; n <=254; n++))
 
 
                         MOSH_PORT=$(($MOSH_BASE))
-                        $echo ${MOSH_PORT}
-                        iptables -t nat -A PREROUTING -i ens3 -p udp -m udp --dport ${MOSH_PORT} -j DNAT --to-destination ${LOCALNET}.$base.${n}:${MOSH_PORT}
-                        iptables -t nat -A POSTROUTING -d ${LOCALNET}.$base.${n}/32 -p udp -m udp --dport ${MOSH_PORT} -j SNAT --to-source ${LOCALIP}
+                        echo ${MOSH_PORT}
+                        iptables -t nat -A PREROUTING -i ens3 -p udp -m udp --dport ${MOSH_PORT} -j DNAT --to-destination ${LOCALNET}.${base}.${n}:${MOSH_PORT}
+                        iptables -t nat -A POSTROUTING -d ${LOCALNET}.${base}.${n}/32 -p udp -m udp --dport ${MOSH_PORT} -j SNAT --to-source ${LOCALIP}
 
 
         }
